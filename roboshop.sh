8b36f676ebd7d712eca60123afc90edb413f7eed
naresh <nareshveeranala@gmail.com>
2025-07-13 16:18:02 +0530

    creating instance

--- roboshop.sh
+++ roboshop.sh
@@ -8,12 +8,12 @@ DOMAIN_NAME="nareshveeranala.shop"
 
 for instance in ${INSTANCES[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-050181a88007d400e --tag-specifications "ResourceType=instance,Tags=[{key=Name,value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-050181a88007d400e --tag-specifications "ResourceType=instance,Tags=[{key=Name,value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         Ip=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
         Ip=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi
-    echo "$instance Ip address:$IP"
+    echo "$instance Ip address: $IP"
 done
