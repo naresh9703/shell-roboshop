9317a957b6d94027fa6c9d38ad8eb7555d33b171
naresh <nareshveeranala@gmail.com>
2025-07-14 18:51:34 +0530

    creating shell scripts

--- roboshop.sh
+++ roboshop.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-050181a88007d400e"
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "payment" "dispatch" "frontend")
-ZONE_ID="Z0072111386H9FAC3M1H8"
-DOMAIN_NAME="nareshveeranala.shop"
+SG_ID="sg-050181a88007d400e" # replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z0072111386H9FAC3M1H8" # replace with your ZONE ID
+DOMAIN_NAME="nareshveeranala.shop" # replace with your domain
 
 for instance in ${INSTANCES[@]}
+#for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-050181a88007d400e --tag-specifications 'ResourceType=instance,Tags=[{key=Name,Value=$instance}]' --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-050181a88007d400e --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
-        Ip=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
-        Ip=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
-    echo "$instance Ip address: $IP"
+    echo "$instance IP address: $IP"
 done
